d88fc0f9f14c121c401ffd9911ed5001fb3fd30c
Mcroww <mario.valenzuela.d@gmail.com>
2026-04-25 11:26:54 -0300

    Initial Spring Boot project setup

---
+++ src/test/java/com/cornerstone/maintenance/MaintenanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cornerstone.maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
